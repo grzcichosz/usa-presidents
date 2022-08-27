ced974ed4171e93f422e69004cd7b6b9f0e296e3
Grzegorz <grzcichosz@gmail.com>
2022-08-27 14:38:48 +0200

    Initial commit

---
+++ src/main/java/com/usapresidents/UsaPresidentsApplication.java
@@ -0,0 +1,13 @@
+package com.usapresidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsaPresidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsaPresidentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usapresidents/UsaPresidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usapresidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsaPresidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
